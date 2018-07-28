bbd9c140e18b44a5ca4fddd66041c64f7b5e5584
atamuhiuldin <atamuhiuldin@gmail.com>
2018-07-28 22:57:25 +0500

    Added Reducers, Created Store, Actions , then Subcribed them and dispatch the action Just Console based

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import rootReducer from './reducers';
+import {createStore} from 'redux';
+import {addCharactorById} from './actions';
+const store = createStore(rootReducer);
+
 
 class App extends Component {
+  state = {
+    collection: ''
+  }
+ 
   render() {
+ 
+    // {store.subscribe(() => { console.log("App.js ", store.getState() ) })}
+
+
     return (
       <div className="App">
         <header className="App-header">
@@ -13,6 +26,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+    
       </div>
     );
   }
